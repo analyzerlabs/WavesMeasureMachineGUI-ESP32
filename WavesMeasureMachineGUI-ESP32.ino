510966bf2ebcf5c4b1078d5dc2407cec8a70c404
MiguelQuispe <fisicomiguel@gmail.com>
2020-10-08 01:22:32 -0500

    firmware

--- WavesMeasureMachineGUI-ESP32.ino
+++ WavesMeasureMachineGUI-ESP32.ino
@@ -3,7 +3,6 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ESP32httpUpdate.h>
-#include "update.h"
 ///////////////// FOR THE MACHINE
 #include <ESP32Encoder.h>
 #include "FS.h"
@@ -61,7 +60,81 @@ void setup(){
 
 //mainloop
 
-int j=0;
+
+
+unsigned long previousMillis = 0;        // will store last time LED was updated
+const long interval = 600000;
+
+ 
+void FirmwareUpdate()
+{
+  http.begin(URL_fw_Version,"CC AA 48 48 66 46 0E 91 53 2C 9C 7C 23 2A B1 74 4D 29 9D 33");     // check version URL
+  delay(100);
+  int httpCode = http.GET();            // get data from version file
+  delay(100);
+  String payload;
+  if (httpCode == HTTP_CODE_OK)         // if version received
+  {
+    payload = http.getString();  // save received version
+    Serial.println(payload );
+  }
+  else
+  {
+    Serial.print("error in downloading version file:");
+    Serial.println(httpCode);
+
+  }
+  
+  http.end();
+  if (httpCode == HTTP_CODE_OK)         // if version received
+  {
+  if(payload.equals(FirmwareVer) )
+  {   
+     Serial.println("Device already on latest firmware version"); 
+  }
+  else
+  {
+     Serial.println("New firmware detected");
+     WiFiClient client;
+
+    // The line below is optional. It can be used to blink the LED on the board during flashing
+    // The LED will be on during download of one buffer of data from the network. The LED will
+    // be off during writing that buffer to flash
+    // On a good connection the LED should flash regularly. On a bad connection the LED will be
+    // on much longer than it will be off. Other pins than LED_BUILTIN may be used. The second
+    // value is used to put the LED on. If the LED is on with HIGH, that value should be passed
+    //ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
+
+
+    t_httpUpdate_return ret = ESPhttpUpdate.update(URL_fw_Bin,"","CC AA 48 48 66 46 0E 91 53 2C 9C 7C 23 2A B1 74 4D 29 9D 33");
+    
+    switch (ret) {
+      case HTTP_UPDATE_FAILED:
+        Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+        break;
+
+      case HTTP_UPDATE_NO_UPDATES:
+        Serial.println("HTTP_UPDATE_NO_UPDATES");
+        break;
+
+      case HTTP_UPDATE_OK:
+        Serial.println("HTTP_UPDATE_OK");
+        break;
+    } 
+  }
+ }  
+}
+void repeatedCall(){
+    unsigned long currentMillis = millis();
+    if ((currentMillis - previousMillis) >= interval) 
+    {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
+      FirmwareUpdate();
+    }
+ }
+
+ int j=0;
 int k=0;
 float multiplier = 2*PI/210;
 void loop(){
